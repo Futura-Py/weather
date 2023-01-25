8bc8b9832209f7f44723d3dbf98387a4e904a478
LG <76845820+im-coder-lg@users.noreply.github.com>
2023-01-25 19:52:19 +0530

    First INDEV commit!

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from time import sleep
+
+import requests
+
+TOKEN = "f2c9ba5917df10d29b7d7f97a4cce627"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+BREAK = "\n\n=====\n\n"
+
+
+def breakliner():
+    print(f"{BREAK:-^30}")
+    return
+
+
+def clearScreen():
+    if sys.platform == "windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    return
+
+
+def exitQuery():
+    exitQ = input("Exit? ")
+    if exitQ == "y":
+        exit()
+    elif exitQ == "n":
+        clearScreen()
+        OWMCITY()
+
+
+def OWMCITY():
+    # City Name request
+    print("Enter City Name (should be in compliance to OpenWeatherMap's City Index) :")
+    CITY = input()
+    if CITY == "exit":
+        clearScreen()
+        print("Three")
+        sleep(1)
+        clearScreen()
+        print("Two")
+        sleep(1)
+        clearScreen()
+        print("One.")
+        sleep(1)
+        clearScreen()
+        print("Exiting...")
+        sleep(0.5)
+        exit()
+
+    else:
+        pass
+    # The URL in actuality be like:
+    URL = BASE_URL + "q=" + CITY + "&appid=" + TOKEN
+
+    # PROD: Request Response
+    resp_PROD = requests.get(URL)
+    if resp_PROD.status_code == 200:
+        pass
+    else:
+        raise TypeError("Oops, wrong city name or code?")
+        sleep(5)
+        exit()
+
+    breakliner()
+    data = resp_PROD.json()
+    print(data)
+    breakliner()
+    return data
+
+
+OWMCITY()
+
+exitQuery()
